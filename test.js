3cb0d060b7bde86056f909f042a9e0fe57f0dbde
Dmitry Semenov <dima.semenov.02@gmail.com>
2025-09-16 17:00:41 +0300

    testing

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-let a =1
-console.log(a)
+let a = 1;
+console.log(a);
 
+const str = 'adasdad';
 
-
-const str='adasdad'
-
-let b = a=4
+let b = (a = 4);
 
 console.log(a);
-console.log(b)
+console.log(b);
+
+const abc = 123;
+console.log(abc);
